dcb190d7bfdbf83c562a9f863cd2d0aa8bd8e4e1
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-02-22 01:21:43 -0300

    🚀 :rocket: [Add] Player life

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function start() {
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
     $("#fundoGame").append("<div id='placar'></div>");
+    $("#fundoGame").append("<div id='energia'></div>");
 
 
     //Principais vari�veis do jogo
@@ -15,6 +16,7 @@ function start() {
     var pontos = 0;
     var salvos = 0;
     var perdidos = 0;
+    var energiaAtual=3;
     var jogo = {}
     var velocidade = 5;
     var posicaoY = parseInt(Math.random() * 334);
@@ -50,6 +52,7 @@ function start() {
         moveamigo();
         colisao();
         placar();
+        energia();
 
     } // Fim da fun��o loop()
 
@@ -171,6 +174,7 @@ function start() {
         // jogador com o inimigo1
 
         if (colisao1.length > 0) {
+            energiaAtual--;
 
             inimigo1X = parseInt($("#inimigo1").css("left"));
             inimigo1Y = parseInt($("#inimigo1").css("top"));
@@ -355,4 +359,30 @@ function start() {
         
     } //fim da fun��o placar()
     
+function energia() {
+	
+    if (energiaAtual==3) {
+        
+        $("#energia").css("background-image", "url(img/energia3.png)");
+    }
+
+    if (energiaAtual==2) {
+        
+        $("#energia").css("background-image", "url(img/energia2.png)");
+    }
+
+    if (energiaAtual==1) {
+        
+        $("#energia").css("background-image", "url(img/energia1.png)");
+    }
+
+    if (energiaAtual==0) {
+        
+        $("#energia").css("background-image", "url(img/energia0.png)");
+        
+        //Game Over
+    }
+
+} // Fim da fun��o energia()
+
 }
